28a3ff8802541d91251129a88c7b568bad8a12c2
Onyekachi Anyanwu <onyekachi@itbps.com>
2018-12-25 16:37:04 -0500

    Updated the code

--- fuelmgmt/src/app/app.module.ts
+++ fuelmgmt/src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthHttp, AUTH_PROVIDERS, provideAuth, AuthConfig } from 'angular2-jwt/angular2-jwt';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { HttpModule, Http, BaseRequestOptions } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { NoAccessComponent } from './no-access/no-access.component'; 
+import {AuthService} from './services/auth.service';
+import { Observable, pipe } from 'rxjs';
+import {  mergeMap } from 'rxjs/operators';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    AdminComponent,
+    NoAccessComponent
     
   ],
   imports: [
     BrowserModule,
-    FormsModule   
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'admin', component: AdminComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'no-access', component: NoAccessComponent }
+    ])    
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- fuelmgmt/src/app/login/login.component.ts
+++ fuelmgmt/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormComponent} from '@angular/forms';
+import {Router}  from '@angular/router';
+import {AuthService} from './../services/auth.service';
+
+
 
 @Component({
   selector: 'login',
@@ -8,8 +11,24 @@ import {FormComponent} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  invalidLogin: boolean; 
+  
+  constructor(private router: Router,
+              private authServices: AuthService)
 
+  { 
+
+  }
+
+  userLogin(credential)
+  {
+     this.authServices.login(credential).subscribe(result=>{
+      if (result)
+         this.router.navigate(['/']);
+      else  
+         this.invalidLogin = true; 
+     }); 
+  }
   ngOnInit() {
   }
 
